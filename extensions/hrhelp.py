a2a6ee2a5b7f8b4feeec539389ef4dd83408cf9d
Brncray <57870234+Brncray@users.noreply.github.com>
2023-04-13 19:47:11 -0400

    Add files via upload

---
+++ extensions/hrhelp.py
@@ -0,0 +1,47 @@
+import lightbulb
+import hikari
+import miru
+import webbrowser
+
+plugin = lightbulb.Plugin('hrhelp', 'HRhelp')
+
+
+
+
+rest = hikari.RESTApp()
+@plugin.listener(hikari.GuildMessageCreateEvent)
+async def print_messages(event):
+    pass
+
+
+
+
+
+
+@plugin.command
+@lightbulb.command('hrhelp', 'HRhelp')
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def ban(ctx):
+  embed = hikari.Embed(
+    title="HRHELP",
+    description="Use the below buttons to access the links.\n1️⃣ - Onboard a new candidate\n2️⃣ - Offboard an Employee \n3️⃣ - Give Raise\n4️⃣ - Change Employment Type\n5️⃣ - Issue Equity to an Employee\n6️⃣ - Run Manual Payroll"
+  )
+  view = miru.View()
+  view.add_item(miru.Button(label="1️⃣", url="https://app.process.st", row=1))
+  view.add_item(miru.Button(label="2️⃣", url="https://app.process.st", row=1))
+  view.add_item(miru.Button(label="3️⃣", url="https://app.process.st", row=1))
+  view.add_item(miru.Button(label="4️⃣", url="https://app.process.st", row=2))
+  view.add_item(miru.Button(label="5️⃣", url="https://app.process.st", row=2))
+  view.add_item(miru.Button(label="6️⃣", url="https://app.process.st", row=2))
+
+  await ctx.respond(embed=embed, components=view)
+
+
+
+
+
+
+
+
+def load(bot):
+    bot.add_plugin(plugin)
---
+++ main.py
@@ -0,0 +1,67 @@
+import hikari
+import lightbulb
+from tinydb import TinyDB, Query
+import os
+token = os.environ['token']
+App = Query()
+db = TinyDB('data.json')
+from threading import Thread
+import miru
+
+
+
+
+
+
+
+bot = lightbulb.BotApp(
+  token=token,
+  help_slash_command=True
+)
+
+
+@bot.listen(lightbulb.CommandErrorEvent)
+async def on_error(event: lightbulb.CommandErrorEvent) -> None:
+    if isinstance(event.exception, lightbulb.CommandInvocationError):
+        await event.context.respond(f"Something went wrong during invocation of command `{event.context.command.name}`.")
+        raise event.exception
+
+    # Unwrap the exception to get the original cause
+    exception = event.exception.__cause__ or event.exception
+
+    if isinstance(exception, lightbulb.NotOwner):
+        await event.context.respond("You are not the owner of this bot.")
+    elif isinstance(exception, lightbulb.CommandIsOnCooldown):
+        time = exception.retry_after /3600
+        import time
+        
+        timestamp = round(time.time()) + exception.retry_after
+        await event.context.respond(f"This command is on cooldown. Retry at <t:{round(time.time() + exception.retry_after)}> ")
+    elif ...:
+        ...
+    else:
+        raise exception
+
+
+
+@bot.listen(hikari.StartedEvent)
+async def bot_started(event):
+  
+    print('Bot has started.')
+
+
+
+
+
+
+
+
+
+
+bot.load_extensions_from('./extensions')
+miru.install(bot)
+print("Miru initialized")
+
+bot.run()
+
+
